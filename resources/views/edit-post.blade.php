605e91fdd6071b1280a7e5b9afc7c7e9dd100f33
Oluwajolasun Jaiyesimi <51948882+oluwajolasun@users.noreply.github.com>
2024-09-07 19:21:12 -0600

    Still battling with tailwind for deployment

--- resources/views/edit-post.blade.php
+++ resources/views/edit-post.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Edit Post</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite('resources/css/app.css')
 </head>
 <body>
     <div class="container mx-auto bg-gray-50">
